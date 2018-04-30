8fea9a11570ad66feef6d9d336662a8289e50f0a
Steve <stevenjameswatkin@gmail.com>
2018-04-30 23:08:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(element)
-
-
+function destructivelyAppendKitten(element) {
+kittens.push(element)
+return kittens
+}
